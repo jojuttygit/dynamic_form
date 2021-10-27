0263b78d5ca662f0e8a056d594232a939795fba6
jojuttygit <jojutty@gmail.com>
2021-10-27 08:59:34 +0530

    Review and Cleaning code

--- resources/views/forms/public/show.blade.php
+++ resources/views/forms/public/show.blade.php
@@ -1,5 +1,4 @@
 @extends('layouts.app')
-
 @section('content')
 <div class="container">
     <div class="row justify-content-center">
@@ -38,8 +37,4 @@
         </div>
     </div>
 </div>
-
-<script type="text/javascript">
-</script>
-
 @endsection
